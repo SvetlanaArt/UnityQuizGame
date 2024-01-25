28e9e49c04b94d2758512743e366dad21d91ec10
Svetlana Artsiomenka <148551551+SvetlanaArt@users.noreply.github.com>
2024-01-25 19:51:43 +0300

    Check answer
    Added ability to check answer, highlighting correct answer and wrong answer.
    Readme updated.

--- Assets/Scripts/QuestionMeneger.cs
+++ Assets/Scripts/QuestionMeneger.cs
@@ -1,17 +1,25 @@
 using System;
+using System.Linq;
 using System.Reflection.Emit;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class QuestionMeneger : MonoBehaviour
 {
     [SerializeField]
+    int rightAnswerScore = 100;
+    [SerializeField]
     TextMeshProUGUI questionText;
     [SerializeField]
     Toggle[] answerButtons;
     [SerializeField]
-    int AnswersCount = 4;
+    int answersCount = 4;
+    [SerializeField]
+    Sprite spriteRightAnswer;
+    [SerializeField]
+    Sprite spriteWrongAnswer;
  
     QuestionData questionData;
     TestManager testManager;
@@ -19,25 +27,26 @@ public class QuestionMeneger : MonoBehaviour
     void Start()
     {
         testManager = FindObjectOfType<TestManager>();
+        answerIsChecked = new bool[answersCount];
         SetQuestion();
-        answerIsChecked = new bool[AnswersCount];
-        Array.ForEach(answerIsChecked, element => element = false);
-
     }
 
 
     void SetQuestion()
     {
+        Text answer;
+        Array.ForEach(answerIsChecked, isChecked => isChecked = false);
         if (testManager != null)
         {
             questionData = testManager.GetNextQuestion();
-            if (questionData != null && questionData.answers.Length >= AnswersCount)
+            if (questionData != null && questionData.answers.Length >= answersCount)
             {
                 questionText.text = questionData.questionText;
-                for (int i = 0; i < AnswersCount; i++)
+                for (int i = 0; i < answersCount; i++)
                 {
-                    Text answer = answerButtons[i].GetComponentInChildren<Text>();
+                    answer = answerButtons[i].GetComponentInChildren<Text>();
                     answer.text = questionData.answers[i];
+                    answerButtons[i].enabled = true;
                 }
 
             }
@@ -51,6 +60,26 @@ public class QuestionMeneger : MonoBehaviour
 
     public void CheckAnswers()
     {
- 
+        bool expectedAnswer;
+        Image imageButton;
+        for (int i = 0; i < answerIsChecked.Length; i++)
+        {
+            answerButtons[i].enabled = false;
+            expectedAnswer = questionData.numbersRightAnswers.Contains<int>(i);
+            if (expectedAnswer)
+            {
+                imageButton = answerButtons[i].GetComponentInChildren<Image>();
+                imageButton.sprite = spriteRightAnswer;
+            }
+            if (answerIsChecked[i] != expectedAnswer)
+            {
+                rightAnswerScore -= rightAnswerScore / answersCount;
+                if (answerIsChecked[i])
+                {
+                    imageButton = answerButtons[i].GetComponentInChildren<Image>();
+                    imageButton.sprite = spriteWrongAnswer;
+                }
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,28 @@
 Test your knowledge with the quiz.
 Small game in Unity (editor ver 2022.3.4f1).
 
+Scripts>
 
+public class QuestionData
+	Used for serialization data from json format to an object of this class.
+
+public class QuestionMeneger : MonoBehaviour
+	Manage one current question.
+	Put data into UI and check answer result.
+
+public interface TestLoader 
+	Interface for load data of test questions.
+
+public class TestFromFileLoader : MonoBehaviour, TestLoader
+	Load json string from file.
+
+public class TestParserJson
+	Parse json string to List<QuestionData>.
+
+public class TestManager : MonoBehaviour
+	Manage Test.
+	Initiate loading question data.
+	Allow to get data of the next question.
 
 Images generated in Bing.
 Font are free for commertial use. https://1001fonts.com/
